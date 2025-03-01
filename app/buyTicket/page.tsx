10764ea317f5c25fe61f9eb16c5832e47d985230
mahdi <m.mazloum75@gmail.com>
2025-03-01 15:59:58 +0330

    refactor BuyTicket form layout and styles; improve responsiveness and update message colors

--- app/buyTicket/page.tsx
+++ app/buyTicket/page.tsx
@@ -46,46 +46,51 @@ const BuyTicket: FC<BuyTicketProps> = () => {
 
   return (
     <div className="h-screen">
-      <Card className="w-4/5 lg:w-2/5 p-8 min-h-auto lg:mx-auto mx-4 mt-16  backdrop-blur-sm bg-gradient-to-r from-orange-400/40 to-orange-600/40 border border-white/20 shadow-[0px_15px_60px_#eb9b3e] rounded-lg">
+      <Card className="w-4/5 md:w-3/5 p-8 min-h-auto align-middle md:mx-auto mx-4 mt-16  backdrop-blur-sm bg-gradient-to-r from-orange-400/40 to-orange-600/40 border border-white/20 shadow-[0px_15px_60px_#eb9b3e] rounded-lg">
+        <h1 className="text-2xl text-white align-middle m-auto mt-5">
+          تهیه بلیط
+        </h1>
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-5  my-14"
+            className="space-y-12  my-5"
           >
             {/** Username */}
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input
-                      placeholder="نام"
-                      {...field}
-                      className="text-right"
-                    />
-                  </FormControl>
-                  <FormMessage className="text-white text-right text-md" />
-                </FormItem>
-              )}
-            />
-            {/** Last Name */}
-            <FormField
-              control={form.control}
-              name="lastName"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input
-                      placeholder="نام خانوادگی"
-                      {...field}
-                      className="text-right"
-                    />
-                  </FormControl>
-                  <FormMessage className="text-white text-right text-md" />
-                </FormItem>
-              )}
-            />
+            <div className="flex flex-col md:flex-row  gap-4 w-full">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormControl>
+                      <Input
+                        placeholder="نام"
+                        {...field}
+                        className="text-right"
+                      />
+                    </FormControl>
+                    <FormMessage className="text-gray-300 text-right text-md" />
+                  </FormItem>
+                )}
+              />
+              {/** Last Name */}
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormControl>
+                      <Input
+                        placeholder="نام خانوادگی"
+                        {...field}
+                        className="text-right"
+                      />
+                    </FormControl>
+                    <FormMessage className="text-gray-300 text-right text-md" />
+                  </FormItem>
+                )}
+              />
+            </div>
             {/** Phone */}
             <FormField
               control={form.control}
@@ -99,7 +104,7 @@ const BuyTicket: FC<BuyTicketProps> = () => {
                       className="text-right"
                     />
                   </FormControl>
-                  <FormMessage className="text-white text-right text-md" />
+                  <FormMessage className="text-gray-300 text-right text-md" />
                 </FormItem>
               )}
             />
@@ -116,7 +121,7 @@ const BuyTicket: FC<BuyTicketProps> = () => {
                       className="text-right"
                     />
                   </FormControl>
-                  <FormMessage className="text-white text-right text-md" />
+                  <FormMessage className="text-gray-300 text-right text-md" />
                 </FormItem>
               )}
             />
